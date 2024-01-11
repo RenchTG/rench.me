5325bca517d5680e4aa4cb83ce2985c5b3c8375d
RenchTG <dobranowskip1@gmail.com>
2024-01-10 18:53:53 -0700

    Revert title change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
-import { genPageMetadata } from 'app/seo'
-
-export const metadata = genPageMetadata({ title: 'Home' })
 
 export default async function Page() {
     const sortedPosts = sortPosts(allBlogs)
