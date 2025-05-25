939523d97fc1c922707535d48990e8e18cc728ff
Philip Dobranowski <dobranowskip1@gmail.com>
2025-05-24 21:24:52 -0400

    Revert "Testing a hypothesis"
    This reverts commit bc3bf52a5a90698de7f863a0f8c2b96e355f6db5.

--- src/consts.ts
+++ src/consts.ts
@@ -27,7 +27,7 @@ export const NAV_LINKS: Link[] = [
   { href: '/', label: 'home' },
   { href: '/about', label: 'about' },
   { href: '/blog', label: 'blog' },
-  { href: 'https://drive.google.com/file/d/17pWDOVYT3xJjryri4AMpAKr44AtB8gp1/view?usp=sharing', label: 'resume' },
+  { href: 'https://drive.google.com/file/d/17pWDOVYT3xJjryri4AMpAKr44AtB8gp1/view?usp=sharing', label: 'resume', external: true },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
