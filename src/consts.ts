29690deadaca1d9025e0b98ca98345177909036f
Philip Dobranowski <dobranowskip1@gmail.com>
2025-07-29 19:09:30 -0400

    New resume link

--- src/consts.ts
+++ src/consts.ts
@@ -27,7 +27,7 @@ export const NAV_LINKS: Link[] = [
   { href: '/', label: 'home' },
   { href: '/about', label: 'about' },
   { href: '/blog', label: 'blog' },
-  { href: 'https://drive.google.com/file/d/17pWDOVYT3xJjryri4AMpAKr44AtB8gp1/view?usp=sharing', label: 'resume', external: true },
+  { href: 'https://drive.google.com/file/d/1lDFpSSpmE0MSJ7Sc00rGXD0Fr8IaIP4h/view?usp=sharing', label: 'resume', external: true },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
